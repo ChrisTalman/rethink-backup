bd89ad73dc539906662a7e7007ae6fa8763c51cc
Chris Talman <cewe.wow@gmail.com>
2019-01-03 20:49:51 +0000

    Moved Manifest types into separate types module.

--- src/Modules/Export/Manifest.ts
+++ src/Modules/Export/Manifest.ts
@@ -7,24 +7,7 @@ import { r as RethinkDB } from 'rethinkdb-ts';
 import RethinkUtilities from 'src/Modules/Utilities/RethinkDB';
 
 // Types
-export interface Manifest
-{
-    moduleVersion: string;
-    databases: Databases;
-};
-export interface Databases extends Array<Database> {};
-export interface Database
-{
-    id: string;
-    name: string;
-    tables: Tables;
-};
-export interface Tables extends Array<Table> {};
-export interface Table
-{
-    id: string;
-    name: string;
-};
+import { Manifest, Databases } from 'src/Types/Manifest';
 
 export default async function generate({directoryPath}: {directoryPath: string})
 {
---
+++ src/Types/Manifest.ts
@@ -0,0 +1,22 @@
+'use strict';
+
+export interface Manifest
+{
+    moduleVersion: string;
+    databases: Databases;
+};
+
+export interface Databases extends Array<Database> {};
+export interface Database
+{
+    id: string;
+    name: string;
+    tables: Tables;
+};
+
+export interface Tables extends Array<Table> {};
+export interface Table
+{
+    id: string;
+    name: string;
+};
