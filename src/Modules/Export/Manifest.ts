abb88b76a2dc43f4623c76f6ba4bb9c0a366fd2c
Chris Talman <cewe.wow@gmail.com>
2019-01-04 12:31:14 +0000

    Filtered databases.

--- src/Modules/Export/Manifest.ts
+++ src/Modules/Export/Manifest.ts
@@ -7,12 +7,14 @@ import { r as RethinkDB } from 'rethinkdb-ts';
 import RethinkUtilities from 'src/Modules/Utilities/RethinkDB';
 
 // Types
+import { RDatum } from 'rethinkdb-ts';
 import { Manifest, Databases } from 'src/Types/Manifest';
-import { Options } from './';
+import { Options, DatabaseFilters } from './';
+interface DatabaseFiltersExpression extends RDatum<DatabaseFilters> {};
 
 export default async function generate({directoryPath, options}: {directoryPath: string, options: Options})
 {
-    const databases = await getDatabases();
+    const databases = await getDatabases(options);
     const manifest: Manifest =
     {
         moduleVersion: MODULE_VERSION,
@@ -25,14 +27,16 @@ export default async function generate({directoryPath, options}: {directoryPath:
     return manifest;
 };
 
-async function getDatabases()
+async function getDatabases(options: Options)
 {
+    const databaseFilters = getFilters(options);
     const query = RethinkDB
         .db('rethinkdb')
         .table('db_config')
+        .filter(database => filterDatabase(database, databaseFilters, options))
         .merge
         (
-            (database: any) =>
+            (database: RDatum) =>
             (
                 {
                     tables: RethinkDB
@@ -46,4 +50,33 @@ async function getDatabases()
         );
     const tables: Databases = await RethinkUtilities.run({query});
     return tables;
+};
+
+function filterDatabase(database: RDatum, filters: DatabaseFiltersExpression, options: Options)
+{
+    const query = RethinkDB
+        .or
+        (
+            filters.typeOf().eq('NULL'),
+            filters.contains(database('name')).eq('pluck' in options)
+        );
+    return query;
+};
+
+function getFilters(options: Options)
+{
+    if (!('pluck' in options) && !('without' in options)) return null;
+    const filters = ('pluck' in options && options.pluck) || ('without' in options && options.without);
+    const flattened = filters.reduce
+    (
+        (names, databaseVariant) =>
+        {
+            if (typeof databaseVariant === 'string') names.push(databaseVariant);
+            else names.push(... Object.keys(databaseVariant));
+            return names;
+        },
+        [] as Array<string>
+    );
+    const expression: DatabaseFiltersExpression = RethinkDB.expr(flattened);
+    return expression;
 };
