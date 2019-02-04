1ea356d5ada11cc690f0d15c55bcba8be9f6856f
Chris Talman <cewe.wow@gmail.com>
2019-02-04 16:05:49 +0000

    Renamed dump() and restore() from export() and import().

--- src/Modules/index.ts
+++ src/Modules/index.ts
@@ -1,5 +1,5 @@
 'use strict';
 
 // Methods
-export { default as export } from 'src/Modules/Export';
-export { default as import } from 'src/Modules/Import';
+export { default as dump } from 'src/Modules/Export';
+export { default as restore } from 'src/Modules/Import';
