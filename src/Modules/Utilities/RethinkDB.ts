20fa1ee45dd4217c8ad9cd2f54ee1f9f32fc6b5d
Chris Talman <cewe.mail@gmail.com>
2019-07-23 13:21:14 +0100

    Removed obsolete module.

--- src/Modules/Utilities/RethinkDB.ts
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-// External Modules
-import { r as RethinkDB } from 'rethinkdb-ts';
-import RethinkUtilities from '@bluecewe/rethink-utilities';
-
-const instance = new RethinkUtilities({RethinkDB});
-export default instance;
