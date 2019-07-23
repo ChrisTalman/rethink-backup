4c2501702f69d16585a779911fb8061774247382
Chris Talman <cewe.mail@gmail.com>
2019-07-23 13:16:50 +0100

    Introduced archive() options argument types.

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,23 @@
+// Types
+import { RConnectionOptions } from 'rethinkdb-ts';
+
 declare module '@bluecewe/rethink-backup'
 {
     /** Exports from database. */
-    export function archive(): Promise<void>;
+    export function archive({options}: {options: ArchiveOptions}): Promise<void>;
+    export interface ArchiveOptions
+    {
+        /** RethinkDB connection options. */
+    	rethink: string | RConnectionOptions;
+        pluck?: DatabaseFilters;
+        without?: DatabaseFilters;
+    }
+    export interface DatabaseFilters extends Array<string | DatabaseFiltersObject> {}
+    export interface DatabaseFiltersObject
+    {
+        [databaseName: string]: TablesFilters;
+    }
+    export interface TablesFilters extends Array<string> {}
     /** Imports into database. */
     export function insert(): Promise<void>;
 }
